56cd86604ae28171b8e3f6fd60bde82c8e9939b0
Ryan Ben S. Villanueva <ryanben.villanueva@gmail.com>
2019-09-10 13:36:14 +0800

    fix: Provide solution to the udaciFamily-quiz

---
+++ README.md
@@ -0,0 +1,4 @@
+# _UdaciFamily Quiz_
+#### _September 10, 2019_
+#### _This is my solution to the udaciFamily quiz from Udacity's free Intro to JavaScript course._
+#### _By Ryan Ben S. Villanueva_
---
+++ udacifamily.js
@@ -0,0 +1,16 @@
+// Directions:
+// Create an array called udaciFamily and add "Julia", "James", and your name to the array. Then, print the udaciFamily to the console using console.log.
+
+/*
+ * Programming Quiz: UdaciFamily (6-1)
+ */
+
+// your code goes here
+
+var udaciFamily = [];
+
+udaciFamily[0] = "Julia";
+udaciFamily[1] = "James";
+udaciFamily[2] = "Ryan";
+
+console.log(udaciFamily);
